68d5d6bdf8b575eec2f072cda68247c75cdb754f
Minkyeongrok <minklok95@gmail.com>
2023-02-08 17:24:34 +0900

    2.8

--- Rok/src/main/java/kr/rok/controller/MenuController.java
+++ Rok/src/main/java/kr/rok/controller/MenuController.java
@@ -1,5 +1,20 @@
 package kr.rok.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.rok.kiosk.service.MenuService;
+
+@Controller
 public class MenuController {
+	@Autowired
+	private MenuService MenuService;
+
+	@GetMapping("/menu")
+	public String selectList() {
+
+		return "menu";
+	}
 
 }
