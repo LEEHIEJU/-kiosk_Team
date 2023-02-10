e53d63fb81ebee705526e49f00cfbc25ceba4fe9
LEE HEE JU <satusna@gmail.com>
2023-02-10 16:03:33 +0900

    hizju update

--- Cafe_Kiosk/src/main/java/kr/hizju/cafekiosk/vo/DrinkSizeVO.java
+++ Cafe_Kiosk/src/main/java/kr/hizju/cafekiosk/vo/DrinkSizeVO.java
@@ -2,30 +2,19 @@ package kr.hizju.cafekiosk.vo;
 
 import javax.swing.Spring;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DrinkSizeVO {
 	private int drinksizeid;
-	private Spring hotsmall; 
-	private Spring icesmall;
-	private Spring hotlarge;
-	private Spring icelarge;
-	
-	public DrinkSizeVO(int drinksizeid, Spring hotsmall, Spring icesmall, Spring hotlarge, Spring icelarge) {
-		super();
-		this.drinksizeid = drinksizeid;
-		this.hotsmall = hotsmall;
-		this.icesmall = icesmall;
-		this.hotlarge = hotlarge;
-		this.icelarge = icelarge;
-	}
-
-	@Override
-	public String toString() {
-		return "DrinkSizeVO [drinksizeid=" + drinksizeid + ", hotsmall=" + hotsmall + ", icesmall=" + icesmall
-				+ ", hotlarge=" + hotlarge + ", icelarge=" + icelarge + "]";
-	}
+	private String hotsmall; 
+	private String icesmall;
+	private String hotlarge;
+	private String icelarge;
 
 	
 }
