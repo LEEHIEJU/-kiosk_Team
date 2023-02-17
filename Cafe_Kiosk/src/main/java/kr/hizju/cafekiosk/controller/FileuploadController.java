234053dca42072b6dfa309f6aedab1adca9a604d
LEE HEE JU <satusna@gmail.com>
2023-02-17 17:12:47 +0900

    fileupload update

--- Cafe_Kiosk/src/main/java/kr/hizju/cafekiosk/controller/FileuploadController.java
+++ Cafe_Kiosk/src/main/java/kr/hizju/cafekiosk/controller/FileuploadController.java
@@ -21,10 +21,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import kr.hizju.cafekiosk.vo.FileuploadVO;
 import lombok.extern.slf4j.Slf4j;
 
@@ -41,10 +44,11 @@ public class FileuploadController {
 			return "uploadForm";
 		}
 
-		@PostMapping("/upload")
+		@PostMapping
 		// 업로드하는 파일들을 MultipartFile 형태의 파라미터로 전달된다.
-		public String upload(@RequestParam MultipartFile[] uploadfile, Model model)
+		public String upload(@RequestParam MultipartFile[] uploadfile, Model model, HttpServletRequest request)
 				throws IllegalStateException, IOException {
+			String files = request.getServletContext().getRealPath(filePath);
 			List<FileuploadVO> list = new ArrayList<>();
 			for (MultipartFile file : uploadfile) {
 				if (!file.isEmpty()) {
@@ -52,13 +56,13 @@ public class FileuploadController {
 					FileuploadVO vo = new FileuploadVO(UUID.randomUUID().toString(), file.getOriginalFilename(), file.getContentType());
 					list.add(vo);
 
-					File newFileName = new File(vo.getId() + "_" + vo.getFilename());
+					File newFileName = new File(vo.getFilename());
 					// 전달된 내용을 실제 물리적인 파일로 저장해준다.
 					file.transferTo(newFileName);
 				}
 			}
 			model.addAttribute("files", list);
-			return "result";
+			return files;
 		}
 
 
